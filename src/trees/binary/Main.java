a460dff7d0b7f6426249f4334bff77d0de561e07
Lucas Andrin <lucas.andrin.programmer@gmail.com>
2023-09-18 21:36:47 -0300

    preguiça

--- src/trees/binary/Main.java
+++ src/trees/binary/Main.java
@@ -8,19 +8,27 @@ public class Main {
             tree.add(i);
         }
 
+        for (int i = 0; i < 3; i++) {
+            tree.add(i);
+        }
+
         System.out.println("Árvore binária: ");
         System.out.println(tree.show());
 
-        System.out.println("3) Escreva um algoritmo que retorna true se uma determinada árvore é uma árvore AVL e false caso contrário? ");
+        System.out.println("2) Escreva o método de exclusão de um elemento da árvore AVL.");
+        tree.remove(10);
+        System.out.println(tree.show());
+
+        System.out.println("3) Escreva um algoritmo que retorna true se uma determinada \n árvore é uma árvore AVL e false caso contrário? ");
         System.out.println("Resultado: " + tree.isBalanced());
 
-        System.out.println("4) Faça uma função que, dada uma árvore AVL, retorne à quantidade de nós que guardam números primos");
+        System.out.println("4) Faça uma função que, dada uma árvore AVL, retorne à \n quantidade de nós que guardam números primos");
         System.out.println("Resultado: " + tree.filter(BinaryNode::isPrimeNumber).count());
 
         System.out.println("6) Escreva um método que receba um nível da árvore e mostre todos os nodos nesse nível.");
         System.out.println(tree.filter(node -> node.level == 3).show());
 
-        System.out.println("7) Faça um método para somar os nós presentes nos níveis ímpares de uma árvore AVL.");
+        System.out.println("7) Faça um método para somar os nós presentes nos níveis \nímpares de uma árvore AVL.");
         System.out.println("Resultado: " + tree.filter(node -> node.level % 2 != 0).sum());
     }
 }
